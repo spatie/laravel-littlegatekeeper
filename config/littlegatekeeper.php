664ec03357928ae396bf6c845d4e51c0981afc75
Jonas Siewertsen <mail@jonassiewertsen.com>
2019-06-17 09:41:52 +0200

    Using the .env file to store the login credentials
    Storing the user information in the .env file feels cleaner to me, but if wanted, it is still possible to save those in the config file only.

--- config/littlegatekeeper.php
+++ config/littlegatekeeper.php
@@ -2,8 +2,8 @@
 
 return [
     // Login credentials
-    'username' => 'username',
-    'password' => 'password',
+    'username' => env('GATEKEEPER_USERNAME', 'default_username'),
+    'password' => env('GATEKEEPER_PASSWORD', 'default_password'),
 
     // The key as which the littlegatekeeper session is stored
     'sessionKey' => 'littlegatekeeper.loggedin',
